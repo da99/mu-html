a2cf6dd5d7991e9c2e4a9173e2b1602e1e10ec90
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-23 08:05:46 -0500

    File renaming.

--- specs/mu-uri.spec.cr
+++ specs/mu-uri.spec.cr
@@ -26,19 +26,23 @@ def inspect_uri(s : String)
   puts "normalize: #{uri.normalize.to_s.inspect}"
 end # === def inspect_uri
 
+INPUT_FILES = [] of String
 if File.file?(DIR + "/input")
   INPUTS << read_file(DIR + "/input")
+  INPUT_FILES << "/input"
 end
 
 if File.file?(DIR + "/multi-input")
   read_file(DIR + "/multi-input").split("\n").each do |line|
     INPUTS << line
+    INPUT_FILES << "/multi-input"
   end
 end
 
 if File.directory?(DIR + "/input")
   Dir.glob(DIR + "/input/*").each do |file|
     INPUTS << read_file(file)
+    INPUT_FILES << "/input/#{File.basename file}"
   end
 end
 
@@ -92,7 +96,7 @@ INPUTS.each_index do |i|
   end
 
   puts inspect_uri(input)
-  puts "RED{{FAILED}}: BOLD{{#{DIR}}}"
+  puts "RED{{FAILED}}: BOLD{{#{DIR}#{INPUT_FILES[i]}}}"
   puts "BOLD{{INPUT}}: #{input.inspect}"
   puts "#{actual.inspect} RED{{!=}} #{expect.inspect}"
   exit 1
